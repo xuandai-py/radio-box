e391e3f0f91f99cb863c3cc09e368d2025af3371
xuandai-py <buinguyenxuandai@gmail.com>
2023-03-04 21:49:15 +0700

    fixed res topbar

--- components/items/base.js
+++ components/items/base.js
@@ -1,23 +1,23 @@
 import { Box, Flex } from '@chakra-ui/react'
 
 
-const Base = ({ children, type }) => {
+const Base = ({ children, type, style }) => {
     switch (type) {
         case 'pomodoro':
             return (
-                <>
-                    <Flex align='center' height={5} bgColor={'hsla(0,0%,7%,.75)'} gap={1} px={1}  color={'#fff'} borderRadius={'md'}>
-                        {children}
-                    </Flex>
-                </>
+
+                <Flex align='center' height={5} bgColor={'hsla(0,0%,7%,.75)'} gap={1} px={1} color={'#fff'} borderRadius={'md'} {...style}>
+                    {children}
+                </Flex>
+
             )
         default:
             return (
-                <>
-                    <Box bgColor={'hsla(0,0%,7%,.75)'} px={1} mx={4} color={'#fff'} borderRadius={'md'}>
-                        {children}
-                    </Box>
-                </>
+
+                <Box bgColor={'hsla(0,0%,7%,.75)'} px={1} mx={4} color={'#fff'} borderRadius={'md'} {...style}>
+                    {children}
+                </Box>
+
             )
     }
 }
--- components/topbar.js
+++ components/topbar.js
@@ -1,7 +1,7 @@
 import {
     Box, Button, IconButton, Container, Flex, forwardRef,
     Icon, Image, Link, Avatar,
-    Stack, Tooltip, useColorModeValue, useBreakpointValue,
+    Stack, Tooltip, useColorModeValue, useBreakpointValue, useDisclosure,
     Popover,
     PopoverTrigger,
     PopoverContent,
@@ -11,11 +11,18 @@ import {
     PopoverArrow,
     PopoverCloseButton,
     PopoverAnchor,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
 import { DragHandleIcon } from '@chakra-ui/icons'
-import { useState } from 'react'
-import { CiCloudDrizzle, CiShare2, CiDeliveryTruck, CiKeyboard, CiTimer, CiTwitter, CiWavePulse1 } from 'react-icons/ci'
+import { useState, useRef } from 'react'
+import { CiSun, CiCloudDrizzle, CiShare2, CiBaseball, CiDeliveryTruck, CiKeyboard, CiTimer, CiTwitter, CiWavePulse1 } from 'react-icons/ci'
 import { SlFire, SlSocialFacebook, SlSocialGithub, SlSocialGoogle, SlSocialInstagram } from 'react-icons/sl'
 import { useThumbContext } from './context/thumb'
 import Pomodoro from './items/pomodoro'
@@ -43,7 +50,7 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
     )
 }
 
-const PlayStaticTrackItem = ({ href, children, ...props }) => {
+const PlayStaticTrackItem = ({ href, children, style }) => {
 
     // const active = path === href
     const inactiveColor = useColorModeValue('whiteAlpha.900', '')
@@ -54,8 +61,12 @@ const PlayStaticTrackItem = ({ href, children, ...props }) => {
             _focus={{ bg: useColorModeValue('#1DB954', '#1DB954') }}
             _active={{ bg: useColorModeValue('#1DB954', '#1DB954') }}
             _hover={{ bg: useColorModeValue('#1DB954', '#1DB954') }}
-            bg='none' >
-            <Icon as={children} boxSize={6} />
+            bg='none' 
+           border={{base: '1px solid', xl: 'none'}}
+           m={{base: 1, xl: 0}}
+           px={2}
+            >
+            <Icon as={children} boxSize={8} />
         </Button>
     )
 }
@@ -64,28 +75,27 @@ const MenuLink = forwardRef((props, ref) => (
     <Link ref={ref} as={NextLink} {...props} />
 ))
 
-const IconWrapper = ({ style, children }) => (
+const IconWrapper = ({ style, children, icon, title }) => (
     <>
         <Flex display={{ base: 'none', xl: 'flex' }} align={'center'} gap={4} style={style}>
             {children}
         </Flex>
 
         <Flex display={{ base: 'flex', xl: 'none' }} align={'center'} gap={4} style={style}>
-
-
             <Popover isLazy>
                 <PopoverTrigger>
-                    <IconButton aria-label='Contact' icon={<DragHandleIcon />} />
+                    <IconButton aria-label='Contact' icon={{ ...icon }} />
                 </PopoverTrigger>
                 <PopoverContent>
-                    <PopoverHeader fontWeight='semibold'>Reach me via: </PopoverHeader>
+                    <PopoverHeader fontWeight='semibold'>{title}</PopoverHeader>
                     <PopoverArrow />
                     <PopoverCloseButton />
                     <PopoverBody>
                         {children}
                     </PopoverBody>
                 </PopoverContent>
-            </Popover> </Flex>
+            </Popover>
+        </Flex>
     </>
 )
 
@@ -93,7 +103,8 @@ const IconWrapper = ({ style, children }) => (
 const Topbar = props => {
     const { path } = props
     const [visible, setVisible] = useState(false)
-
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = useRef()
     const onClick = () => setVisible(!visible)
     return (
         <Box
@@ -114,30 +125,26 @@ const Topbar = props => {
                 maxW="container.xl"
                 wrap="wrap"
                 align="center"
-                justify="center"
+                justifyContent={'space-between'}
+                gap={2}
             >
                 <Flex align="center" >
-
                     <Image src='https://res.cloudinary.com/dxhl09emw/image/upload/v1677746843/radio/Frame_16_kqw1wj.svg' boxSize={'40px'} objectFit='cover' />
                 </Flex>
                 <Stack
                     direction={{ base: 'row', md: 'row' }}
-                    // display={{ base: 'none', md: 'flex' }}
-                    // width={{ base: 'full', md: 'auto' }}
+                    display={{ base: 'none', sm: 'flex' }}
                     alignItems="center"
-                    // justify="center"
                     flexGrow={1}
-                    // mt={{ base: 4, md: 0 }}
                     spacing={4}
 
                 >
                     <Flex flex={2} align='center' justify='right' gap={4}>
                         {visible && (
-                            
                             <Pomodoro />
                         )}
-                        <Tooltip hasArrow label='Pomodoro' bg='#1DB954' display={{base: 'none', md: 'inline-block'}}>
-                    
+                        <Tooltip hasArrow label='Pomodoro' bg='#1DB954' display={{ base: 'none', md: 'inline-block' }}>
+
                             <IconButton border={'1px solid #1DB954'} bg={'none'} fontSize={20} color={'#1DB954'} onClick={onClick} aria-label='Pomodoro' size='md' icon={<CiTimer />} />
 
                         </Tooltip>
@@ -147,6 +154,8 @@ const Topbar = props => {
                                 paddingTop: useBreakpointValue({ base: 0, xl: '6px' }),
                                 justifyContent: 'right'
                             }}
+                            icon=<CiShare2 />
+                            title='Reach me via: '
                         >
                             <LinkItem
                                 href="https://github.com/xuandai-py"
@@ -174,10 +183,8 @@ const Topbar = props => {
                             </LinkItem>
                         </IconWrapper>
                     </Flex>
-                    <Clock color='#fff' align='center' />
-
-
-                    <IconWrapper style={{ flex: 2, justifyContent: 'left' }} >
+                    <Clock color='#fff' align='center' style={{ display: useBreakpointValue({ base: 'none', sm: 'block' }, { fallback: 'sm' }) }} />
+                    <IconWrapper style={{ flex: 2, justifyContent: 'left' }} icon=<DragHandleIcon /> title='Static tracks: ' >
                         {data.map((item, index) => (
                             <PlayStaticTrackItem key={index}
                                 href={item.href}
@@ -188,7 +195,79 @@ const Topbar = props => {
                         ))}
                     </IconWrapper>
                 </Stack>
+
+                <Drawer
+                    isOpen={isOpen}
+                    placement='bottom'
+                    onClose={onClose}
+                    finalFocusRef={btnRef}
+                >
+                    <DrawerOverlay />
+                    <DrawerContent>
+                        <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+                        <DrawerBody>
+                            <Flex  align='center' gap={4}>
+                                {visible && (
+                                    <Pomodoro />
+                                )}
+                                <Tooltip hasArrow label='Pomodoro' bg='#1DB954' display={{ base: 'none', md: 'inline-block' }}>
+
+                                    <IconButton border={'1px solid #1DB954'} bg={'none'} fontSize={20} color={'#1DB954'} onClick={onClick} aria-label='Pomodoro' size='md' icon={<CiTimer />} />
+
+                                </Tooltip>
+
+                                <IconWrapper
+                                    style={{
+                                        paddingTop: useBreakpointValue({ base: 0, xl: '6px' }),
+                                        justifyContent: 'right'
+                                    }}
+                                    icon=<CiShare2 />
+                                    title='Reach me via: '
+                                >
+                                    <LinkItem
+                                        href="https://github.com/xuandai-py"
+                                        path={path}
+                                    >
+                                        {SlSocialGithub}
+                                    </LinkItem>
+                                    <LinkItem
+                                        href="https://www.facebook.com/profile.php?id=100006465675143"
+                                        path={path}
+                                    >
+                                        {SlSocialFacebook}
+                                    </LinkItem>
+                                    <LinkItem
+                                        href="https://www.instagram.com/hermit_crab_1606"
+                                        path={path}
+                                    >
+                                        {SlSocialInstagram}
+                                    </LinkItem>
+                                    <LinkItem
+                                        href="mailto:buinguyenxuandai@gmail.com"
+                                        path={path}
+                                    >
+                                        {SlSocialGoogle}
+                                    </LinkItem>
+                                </IconWrapper>
+                           
+                            <IconWrapper  icon=<DragHandleIcon /> title='Static tracks: ' >
+                                {data.map((item, index) => (
+                                    <PlayStaticTrackItem key={index}
+                                        href={item.href}
+                                    >
+                                        {item.icon}
+                                    </PlayStaticTrackItem>
+
+                                ))}
+                            </IconWrapper>
+                            </Flex>
+
+                        </DrawerBody>
+                    </DrawerContent>
+                </Drawer>
                 <Flex align="center" >
+                    <IconButton display={{ sm: 'none' }} ref={btnRef} onClick={onOpen} icon={<DragHandleIcon />} mr={2} />
+
                     <ThemeToggleButton />
                 </Flex>
             </Container>
@@ -207,7 +286,7 @@ const data = [
     },
     {
         href: 'https://res.cloudinary.com/dxhl09emw/video/upload/v1677604911/radio/campfire-crackling-fireplace-sound-119594_t0ij1i.mp3',
-        icon: SlFire
+        icon: CiSun
     },
     {
         href: 'https://res.cloudinary.com/dxhl09emw/video/upload/v1677604892/radio/bird-call-in-spring-6865_gq0lqv.mp3',
@@ -220,6 +299,10 @@ const data = [
     {
         href: 'https://res.cloudinary.com/dxhl09emw/video/upload/v1677853743/radio/driving-in-a-car-6227_v5eveh.mp3',
         icon: CiDeliveryTruck
+    },
+    {
+        href: 'https://res.cloudinary.com/dxhl09emw/video/upload/v1677927976/radio/y2mate.com_-_Need_To_Sleep_Try_This_Angelo_Shoe_Shine_ASMR_ribng3.mp3',
+        icon: CiBaseball
     }
 ]
 
